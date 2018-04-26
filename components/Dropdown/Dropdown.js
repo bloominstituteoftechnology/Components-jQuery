9e9481cc86af9804d4e744a7fb783a37c4d3ea1d
mr-burton <taylormade800@gmail.com>
2018-04-26 12:59:27 -0700

    tweaking with the code working on getting functionality working

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,16 +1,15 @@
-let dropDown = $('.dropdown');
-
 class Dropdown {
-  constructor(element) {
-    this.element = $(element);
+  constructor($element) {
+    this.element = (element);
     // Select button and content from the element
     this.button = element.button;
     this.content = element.content;
     // Add a click handler to the button
-    this.element.click(() => { this.dropdown }};
+    this.element.click(() => { this.toggleContent() })};
 
   toggleContent() {
     // Add/remove class to/from content
+    this.element.toggleClass('dropdown');
   }
 }
 
