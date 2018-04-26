5f2a5a0b0fd5d103a8e3b9b58339b879f161659d
AmyShackles <amyshackles@gmail.com>
2018-04-26 12:59:07 -0700

    Changed Dropdown.js
    - I checked to make sure that mixins.less was integrated into the code
    - There didn't seem to be an indication of it in index.css, but the colors seemed to have been in the file, so I'll let it go.

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,17 +1,19 @@
 class Dropdown {
-  constructor($element) {
-    this.element 
+  constructor (element) {
+    this.element = $(element);
     // Select button and content from the element
-    this.button
-    this.content 
+    this.button = $('.dropdown-button');
+    this.content = $('.dropdown-content');
     // Add a click handler to the button
-
+    this.element.click(() => { this.toggleContent() });
   }
 
   toggleContent() {
     // Add/remove class to/from content
+    this.content.toggleClass('dropdown-hidden')
   }
 }
-
-let $dropdown = $('.dropdown');
-$dropdown = new Dropdown($dropdown);
+let dropdown = $('.dropdown')
+dropdown = dropdown.map(function(index, element) {
+  new Dropdown(element);
+});
