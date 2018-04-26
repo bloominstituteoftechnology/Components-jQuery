ce837439450af6819f74edb265c52b09c0b5315e
Niloc92 <prestonresult@github.com>
2018-04-26 18:04:57 -0500

    modified tabs

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,16 +1,21 @@
 class Dropdown {
-  constructor($element) {
-    this.element 
+  constructor(element) {
+    this.element = $(element);// 
     // Select button and content from the element
-    this.button
-    this.content 
+    this.button = this.element;
+    this.content = this.element;
     // Add a click handler to the button
-
+    this.button.click(() => {
+      this.toggleContent();
+    });
   }
 
   toggleContent() {
     // Add/remove class to/from content
+    let dropdownContent = $(".dropdown-content");
+    dropdownContent.toggleClass("dropdown-hidden");
   }
+
 }
 
 let $dropdown = $('.dropdown');
