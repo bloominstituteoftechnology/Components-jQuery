90112777fa368664cd5796e1b1f4b11e5f9dbd02
Anne Courtney <annecourtney_1@yahoo.com>
2018-04-26 14:57:51 -0500

    Completed step 1 of project. Added the dropdown function.

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,17 +1,24 @@
 class Dropdown {
-  constructor($element) {
-    this.element 
+  constructor (element) {
+    this.element = $(element);
     // Select button and content from the element
-    this.button
-    this.content 
+    this.button = $('.dropdown-button');
+    this.content = $('.dropdown-content');
     // Add a click handler to the button
-
+    this.element.click(() => {
+      this.toggleContent()
+    })
   }
 
   toggleContent() {
+    this.content.toggleClass('dropdown-hidden')
     // Add/remove class to/from content
   }
 }
 
-let $dropdown = $('.dropdown');
-$dropdown = new Dropdown($dropdown);
+let dropdown = $('.dropdown')
+
+dropdown = dropdown.map(function(index, element) {
+  new Dropdown(element);
+});
+
