91f69dd1a88a4667ad63c6a1edead192116324b3
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-04-27 03:20:26 +0800

    Working on Dropdown.js. Trying to get the Dropdown Menu to work.

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,17 +1,20 @@
 class Dropdown {
   constructor($element) {
-    this.element 
+    this.element = $(element);
     // Select button and content from the element
-    this.button
-    this.content 
+    this.button = $('.dropdown-button');
+    this.content = $('.dropdown-content');
     // Add a click handler to the button
-
+    this.button.click(() => { this.toggleContent() });
   }
 
   toggleContent() {
     // Add/remove class to/from content
+    this.button.toggleClass("dropdown-hidden");
+
   }
 }
 
 let $dropdown = $('.dropdown');
 $dropdown = new Dropdown($dropdown);
+
