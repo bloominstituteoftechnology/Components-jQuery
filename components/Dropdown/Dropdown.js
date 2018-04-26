a3c6e6ffac2bb2e8f9c79cfc01ba4563e7a9cc2a
mr-burton <taylormade800@gmail.com>
2018-04-26 12:16:57 -0700

    start of the project with dropdown

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,12 +1,13 @@
-class Dropdown {
-  constructor($element) {
-    this.element 
-    // Select button and content from the element
-    this.button
-    this.content 
-    // Add a click handler to the button
+let dropDown = $('.dropdown');
 
-  }
+class Dropdown {
+  constructor(element) {
+    this.element = $(element);
+    // Select button and content from the element
+    this.button = element.button;
+    this.content = element.content;
+    // Add a click handler to the button
+    this.element.click(() => { this.dropdown }};
 
   toggleContent() {
     // Add/remove class to/from content
