d2183518ae32c5138f5c8c35b08e1dbfabda1f96
Brandon Hopper <BrandonHopper0@gmail.com>
2018-04-26 16:49:46 -0400

    compelted  Dropdown.js file

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,17 +1,24 @@
+let $dropdown = $('.dropdown');
+
+
 class Dropdown {
   constructor($element) {
-    this.element 
+    this.element = $(element);
     // Select button and content from the element
-    this.button
-    this.content 
+    this.button  = $(this.element).find('.dropdown-button');
+    this.content = $(this.element).find('.dropdown-content');
     // Add a click handler to the button
-
+    $(this.button).click(() => {this.toggleContent()});
   }
 
   toggleContent() {
     // Add/remove class to/from content
+    $(this.content).toggleClass('dropdown-content');
   }
 }
 
-let $dropdown = $('.dropdown');
-$dropdown = new Dropdown($dropdown);
+
+
+$dropdown = $dropdown.map(function(index, element){
+
+}) = new Dropdown(element);
