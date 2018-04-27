99f48b03a4aa962352d31f9ff2873b6a350eb29d
april <martinezapril56@yahoo.com>
2018-04-26 23:25:33 -0400

    commit

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,10 +1,13 @@
+
+
+
 class Dropdown {
   constructor($element) {
     this.element = $($element);
     // Select button and content from the element
-    this.button = $(".dropdown-button");
+    this.button = $element.find(".dropdown-button");
 
-    this.content = $(".dropdown-content");
+    this.content = $element.find(".dropdown-content");
     // Add a click handler to the button
 
 
